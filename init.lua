91c4e1ba2417b95850b20e3f3b258ce8c6af8df8
Linell Bonnette <tlbonnette@gmail.com>
2014-09-03 22:19:57 -0500

    New mjolnir junk

--- init.lua
+++ init.lua
@@ -1,41 +1,43 @@
-mjolnir.application = require "mjolnir.application"
-mjolnir.window      = require "mjolnir.window"
-mjolnir.hotkey      = require "mjolnir.hotkey"
-mjolnir.fnutils     = require "mjolnir.fnutils"
-grid          = require "grid"
-
-
+local application = require "mjolnir.application"
+local window = require "mjolnir.window"
+local hotkey = require "mjolnir.hotkey"
+local fnutils = require "mjolnir.fnutils"
+local alert = require "mjolnir.alert"
+local grid = require "mjolnir.sd.grid"
 
 local mash = {"cmd", "alt", "ctrl"}
 local mashshift = {"cmd", "alt", "shift"}
 
 local function opendictionary()
-  mjolnir.application.launchorfocus("Dictionary")
+  alert.show("Lexicon, at your service.", 0.75)
+  application.launchorfocus("Dictionary")
 end
 
-mjolnir.hotkey.bind(mash, 'D', opendictionary)
+hotkey.bind(mash, 'D', opendictionary)
 
-mjolnir.hotkey.bind(mash, ';', function() grid.snap(mjolnir.window.focusedwindow()) end)
-mjolnir.hotkey.bind(mash, "'", function() mjolnir.fnutils.map(mjolnir.window.visiblewindows(), grid.snap) end)
+hotkey.bind(mash, ';', function() grid.snap(window.focusedwindow()) end)
+hotkey.bind(mash, "'", function() fnutils.map(window.visiblewindows(), grid.snap) end)
 
-mjolnir.hotkey.bind(mash, '=', function() grid.adjustwidth( 1) end)
-mjolnir.hotkey.bind(mash, '-', function() grid.adjustwidth(-1) end)
+hotkey.bind(mash, '=', function() grid.adjustwidth( 1) end)
+hotkey.bind(mash, '-', function() grid.adjustwidth(-1) end)
 
-mjolnir.hotkey.bind(mashshift, 'H', function() mjolnir.window.focusedwindow():focuswindow_west() end)
-mjolnir.hotkey.bind(mashshift, 'L', function() mjolnir.window.focusedwindow():focuswindow_east() end)
-mjolnir.hotkey.bind(mashshift, 'K', function() mjolnir.window.focusedwindow():focuswindow_north() end)
-mjolnir.hotkey.bind(mashshift, 'J', function() mjolnir.window.focusedwindow():focuswindow_south() end)
+hotkey.bind(mashshift, 'H', function() window.focusedwindow():focuswindow_west() end)
+hotkey.bind(mashshift, 'L', function() window.focusedwindow():focuswindow_east() end)
+hotkey.bind(mashshift, 'K', function() window.focusedwindow():focuswindow_north() end)
+hotkey.bind(mashshift, 'J', function() window.focusedwindow():focuswindow_south() end)
 
-mjolnir.hotkey.bind(mash, 'M', grid.maximize_window)
+hotkey.bind(mash, 'M', grid.maximize_window)
 
-mjolnir.hotkey.bind(mash, 'N', grid.pushwindow_nextscreen)
-mjolnir.hotkey.bind(mash, 'P', grid.pushwindow_prevscreen)
+hotkey.bind(mash, 'N', grid.pushwindow_nextscreen)
+hotkey.bind(mash, 'P', grid.pushwindow_prevscreen)
 
-mjolnir.hotkey.bind(mash, 'J', grid.pushwindow_down)
-mjolnir.hotkey.bind(mash, 'K', grid.pushwindow_up)
-mjolnir.hotkey.bind(mash, 'H', grid.pushwindow_left)
-mjolnir.hotkey.bind(mash, 'L', grid.pushwindow_right)
+hotkey.bind(mash, 'J', grid.pushwindow_down)
+hotkey.bind(mash, 'K', grid.pushwindow_up)
+hotkey.bind(mash, 'H', grid.pushwindow_left)
+hotkey.bind(mash, 'L', grid.pushwindow_right)
 
-mjolnir.hotkey.bind(mash, 'U', grid.resizewindow_taller)
-mjolnir.hotkey.bind(mash, 'O', grid.resizewindow_wider)
-mjolnir.hotkey.bind(mash, 'I', grid.resizewindow_thinner)
+hotkey.bind(mash, 'U', grid.resizewindow_taller)
+hotkey.bind(mash, 'O', grid.resizewindow_wider)
+hotkey.bind(mash, 'I', grid.resizewindow_thinner)
+
+alert.show("Mjolnir, at your service.")

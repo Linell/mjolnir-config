fb2a37814b4a7957893df6addde2e0d83b7648df
Linell Bonnette <tlbonnette@gmail.com>
2015-01-02 10:39:32 -0600

    Stylistic Changes and New Functionality
    I made stuff line up better so that it's easier to read.
    
    More importantly, I added the ability to make a window full screen *or*
    un-fullscreen. I also added the ability to minimize a window. I *almost*
    went with just flat out hiding the application that a window is running,
    but I have no real idea on how to get that back. I think minimize works
    just fine for what all I have in mind.

--- init.lua
+++ init.lua
@@ -1,21 +1,21 @@
 -- Load Extensions
 local application = require "mjolnir.application"
-local window = require "mjolnir.window"
-local hotkey = require "mjolnir.hotkey"
-local fnutils = require "mjolnir.fnutils"
-local alert = require "mjolnir.alert"
-local grid = require "mjolnir.bg.grid"
+local window      = require "mjolnir.window"
+local hotkey      = require "mjolnir.hotkey"
+local fnutils     = require "mjolnir.fnutils"
+local alert       = require "mjolnir.alert"
+local grid        = require "mjolnir.bg.grid"
 -- Music controls
-local spotify = require "mjolnir.lb.spotify"
+local spotify     = require "mjolnir.lb.spotify"
 
 -- Set up hotkey combinations
-local mash = {"cmd", "alt", "ctrl"}
+local mash      = {"cmd", "alt", "ctrl"}
 local mashshift = {"cmd", "alt", "shift"}
 -- Set grid size.
 grid.GRIDWIDTH  = 6
 grid.GRIDHEIGHT = 4
-grid.MARGINX = 0
-grid.MARGINY = 0
+grid.MARGINX    = 0
+grid.MARGINY    = 0
 
 local function opendictionary()
   alert.show("Lexicon, at your service.", 0.75)
@@ -27,17 +27,21 @@ hotkey.bind(mash, 'D', opendictionary)
 hotkey.bind(mash, ';', function() grid.snap(window.focusedwindow()) end)
 hotkey.bind(mash, "'", function() fnutils.map(window.visiblewindows(), grid.snap) end)
 
-hotkey.bind(mash, '=', function() grid.adjustwidth( 1) end)
-hotkey.bind(mash, '-', function() grid.adjustwidth(-1) end)
+hotkey.bind(mash,      '=', function() grid.adjustwidth(1) end)
+hotkey.bind(mash,      '-', function() grid.adjustwidth(-1) end)
 hotkey.bind(mashshift, '=', function() grid.adjustheight(1) end)
 hotkey.bind(mashshift, '-', function() grid.adjustheight(-1) end)
 
-hotkey.bind(mashshift, 'left', function() window.focusedwindow():focuswindow_west() end)
+hotkey.bind(mashshift, 'left',  function() window.focusedwindow():focuswindow_west() end)
 hotkey.bind(mashshift, 'right', function() window.focusedwindow():focuswindow_east() end)
-hotkey.bind(mashshift, 'up', function() window.focusedwindow():focuswindow_north() end)
-hotkey.bind(mashshift, 'down', function() window.focusedwindow():focuswindow_south() end)
+hotkey.bind(mashshift, 'up',    function() window.focusedwindow():focuswindow_north() end)
+hotkey.bind(mashshift, 'down',  function() window.focusedwindow():focuswindow_south() end)
 
 hotkey.bind(mash, 'M', grid.maximize_window)
+hotkey.bind(mashshift, 'M', function() window.focusedwindow():minimize() end)
+
+hotkey.bind(mash,      'F', function() window.focusedwindow():setfullscreen(true) end)
+hotkey.bind(mashshift, 'F', function() window.focusedwindow():setfullscreen(false) end)
 
 hotkey.bind(mash, 'N', grid.pushwindow_nextscreen)
 hotkey.bind(mash, 'P', grid.pushwindow_prevscreen)
@@ -53,9 +57,9 @@ hotkey.bind(mash, 'I', grid.resizewindow_thinner)
 hotkey.bind(mash, 'Y', grid.resizewindow_shorter)
 
 hotkey.bind(mashshift, 'space', spotify.displayCurrentTrack)
-hotkey.bind(mashshift, 'p', spotify.play)
-hotkey.bind(mashshift, 'o', spotify.pause)
-hotkey.bind(mashshift, 'n', spotify.next)
-hotkey.bind(mashshift, 'i', spotify.previous)
+hotkey.bind(mashshift, 'P',     spotify.play)
+hotkey.bind(mashshift, 'O',     spotify.pause)
+hotkey.bind(mashshift, 'N',     spotify.next)
+hotkey.bind(mashshift, 'I',     spotify.previous)
 
-alert.show("Mjolnir, at your service.")
+alert.show("Welcome back, Linell.")
